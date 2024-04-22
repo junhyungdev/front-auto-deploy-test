36ee0f96ed2e28cf0b5b5ef06e03764e02f5f799
junhyung park <87253932+junhyungdev@users.noreply.github.com>
2024-04-22 22:37:31 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,3 @@
 # 1. 현재 실행중인 nginx가 있다면 중지
 sudo service nginx stop
 
-# 2. /home/ubuntu/front-auto-build 디렉토리의 build 디렉토리 안의 모든 것을 /var/www/html로 복사
-sudo cp -r /home/ubuntu/front-auto-build/build/* /var/www/html/
-
-# 3. nginx 실행
-sudo service nginx start
